48b2f04da3f7faefe70a8bb2cb28752ce7eaa63d
Joshua Frankle <joshuafrankle7@gmail.com>
2021-09-25 16:31:28 +0530

    Logout code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import firebase from "firebase/app";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
+  const history = useHistory();
+  function logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        history.push("/");
+      })
+      .catch((error) => {});
+  }
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div className="landing-main">
+        <div className="text-center">
+          <h1>Home Page</h1>
+          <button className="btn btn-primary" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
