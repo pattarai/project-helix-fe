47b4c9f3b3eaa239ce010818474162cfd6d3004a
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-10-17 11:08:58 +0530

    Added class for bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,17 +15,33 @@ export default function Home() {
   }
   return (
     <>
-      <div className="landing-main">
-        <div className="text-center">
-          <h1>Hey Welcome to pattarai's workshopHub!</h1>
-          <button className="btn btn-primary" onClick={logout}>
-            Logout
-          </button>
-          <button onClick={()=>{history.push("/live")}}> Live </button>
-          <button onClick={()=>{history.push("/record")}}>PRE</button>
-          
+      <section className="home-bg full-vh">
+        <div className="landing-main">
+          <div className="text-center">
+            <h1>Hey Welcome to pattarai's workshopHub!</h1>
+            <button className="btn btn-primary" onClick={logout}>
+              Logout
+            </button>
+            <button
+              className="btn btn-secondary"
+              onClick={() => {
+                history.push("/live");
+              }}
+            >
+              {" "}
+              Live{" "}
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                history.push("/record");
+              }}
+            >
+              PRE
+            </button>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
