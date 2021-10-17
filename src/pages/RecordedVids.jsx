557699fd38f3e98d85ca53514705fbe09949c0ca
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-10-17 17:21:15 +0530

    Added vids from yt

--- src/pages/RecordedVids.jsx
+++ src/pages/RecordedVids.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function RecordedVids() {
     return (
         <div>
-         <h1>Hi Check out previous videos</h1>   
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/mk4sVUKkYkc?list=PLLCdGWbcw9uwhUPaCmtQlHlMKyE6R7a1P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
