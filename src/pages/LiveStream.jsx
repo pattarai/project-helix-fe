e8906bd8603ca4ea0b00df948d9f396dcd59878a
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-10-21 10:47:37 +0530

    Imported api but not working

--- src/pages/LiveStream.jsx
+++ src/pages/LiveStream.jsx
@@ -1,60 +1,51 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Marquee from "react-fast-marquee";
 
 function LiveStream() {
+  const { REACT_APP_YOUTUBE_API_KEY } = process.env;
+  const [videoItems, setVideoItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&playlistId=PLLCdGWbcw9uwhUPaCmtQlHlMKyE6R7a1P&key=${REACT_APP_YOUTUBE_API_KEY}`
+      )
+      .then((res) => {
+        const videoitems = res.data.items;
+        setVideoItems(videoitems);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <section className="live-bg full-vh">
-      <div className="stream">
-        <div className="stream__container">
-          <div className="stream__status">
-            <div className="stream__statusContainer">
-              <div className="stream__statusContainerTop">
-                <div className="stream__statusIndicator">LIVE!!</div>
-                <h2>Check out the below streams from PATTARAI</h2>
-              </div>
-              <div className="stream__statusInfo">
-                <i className="fas fa-bell"></i>
-                <p>You will be notified when PATTARAI is Live!</p>
-              </div>
-            </div>
-          </div>
-          <div className="stream__videoEmbed">
-            <iframe
-              width="1280"
-              height="500"
-              src="https://www.youtube.com/embed/mg7FweYjasE"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div>
-      </div>
-     
-      
-      
-        <div>
-          <h1>Description about...</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo
-            nec ultrices dui sapien eget mi proin sed libero. Etiam sit amet
-            nisl purus. Porttitor eget dolor morbi non arcu risus quis varius.
-            Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci.
-            Adipiscing tristique risus nec feugiat in fermentum posuere urna
-            nec. Pretium viverra suspendisse potenti nullam. Gravida arcu ac
-            tortor dignissim convallis aenean et tortor at. Morbi tristique
-            senectus et netus et malesuada fames. Tellus pellentesque eu
-            tincidunt tortor aliquam. Eu scelerisque felis imperdiet proin
-            fermentum leo vel. Nulla at volutpat diam ut. Egestas sed tempus
-            urna et pharetra. At consectetur lorem donec massa sapien faucibus
-            et molestie ac. Dolor morbi non arcu risus quis. Quam pellentesque
-            nec nam aliquam sem.
-          </p>
-        </div>
-      </section>
-    
-  );
+    <>
+    {videoItems.length >= 1 && (
+      <Marquee
+        direction="right"
+        speed={30}
+        gradient
+        gradientWidth={0}
+        gradientColor={[31, 31, 31]}
+      >
+        {videoItems.map((videos, id) => (
+          <button
+            key={id}
+            onClick={() => {
+              window.scrollTo(0, 0);
+              var livestream = document.getElementById("youtubeLive");
+              livestream.style.display = "block";
+              document.getElementById("no-stream").style.display = "none";
+              livestream.src =
+                "https://www.youtube.com/embed/" +
+                videos.contentDetails.videoId;
+            }}
+          >
+          </button>
+        ))}
+      </Marquee>
+    )}
+  </>
+  )
 }
 
-export default LiveStream;
+export default LiveStream
