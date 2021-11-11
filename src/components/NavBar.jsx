f993eae4db901f8dbec6378613268bdc4bca7a5c
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-11-11 20:16:45 +0530

    Added button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 export default function NavBar() {
   return (
     <>
-      <div className="user-name">
-        <p>Joshua</p>
+      <div className="nav-bg full-vh">
+        <p className="btn justify-content-end m-3 px-3 py-2">Helix</p>
       </div>
     </>
   );
