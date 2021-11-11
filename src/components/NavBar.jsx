9a2cca822fcc4f316546eafe9f072d0db0dedaac
Joshua Frankle <joshuafrankle7@gmail.com>
2021-11-11 20:46:58 +0530

    Navbar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { Navbar, Nav } from "react-bootstrap";
 
 export default function NavBar() {
   return (
     <>
-      <div className="nav-bg full-vh">
-        <p className="btn justify-content-end m-3 px-3 py-2">Helix</p>
-      </div>
+      <Navbar bg="light" expand="lg" className="mx-2">
+        <Navbar.Brand href="#home">HELIX</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#">Go Live</Nav.Link>
+            <Nav.Link href="#">Recorded videos</Nav.Link>
+            <Nav.Link href="#">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   );
 }
