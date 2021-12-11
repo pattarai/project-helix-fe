1ac0b5b7ed288acf144300e6e0e8191f5b8f7634
Abishek002-P <abishekprabakaran2003@gmail.com>
2021-12-11 22:19:10 +0530

    added redirect button to recorded page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+
 import { Navbar, Nav } from "react-bootstrap";
+import firebase from "firebase/app";
+import { useHistory } from "react-router-dom";
 
 export default function NavBar() {
+  const history = useHistory();
+  function logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        history.push("/");
+      })
+      .catch((error) => {});
+  }
   return (
     <>
       <Navbar bg="light" expand="lg" className="mx-2">
@@ -10,8 +22,19 @@ export default function NavBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#">Go Live</Nav.Link>
-            <Nav.Link href="#">Recorded videos</Nav.Link>
-            <Nav.Link href="#">Logout</Nav.Link>
+            <button
+              className="btn btn-danger btn-md col-xs-2 margin-left "
+              onClick={() => {
+                history.push("/record");
+              }}
+            >
+              Recent Broadcasts!
+            </button>
+            <div>
+              <div className="btn btn-danger btn-md col-xs-2 margin-left" onClick={logout}>
+              Log out
+            </div>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
