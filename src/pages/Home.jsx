6a19ce23f05046a3678dd352d9f7816a4389ad7c
Crispin R <crispinr003@gmail.com>
2021-12-18 19:15:04 +0530

    Update gif fullscreen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import firebase from "firebase/app";
 import { useHistory } from "react-router-dom";
 // import LiveStream from "./LiveStream";
-import helix_bg from "../assets/images/helix_bg.gif"
-
-
-
-
+import helix_bg from "../assets/images/helix_bg.gif";
 
 export default function Home() {
   const history = useHistory();
@@ -23,23 +19,29 @@ export default function Home() {
       <section className="home-bg full-vh">
         <div className="landing-main">
           <div className="text-center">
-            <h1 class="main">Hey Welcome to Helix!</h1>
-            <button class="btn btn-primary btn-md col-xs-2 margin-left" onClick={logout}>
-              Logout
-            </button>
-            <button
-              className="btn btn-secondary btn-md col-xs-2 margin-left"
-              onClick={() => {
-                history.push("/live");
-              }}
-            >
-              {" "}
-              Live{" "}
-            </button>
-            <div><img src={helix_bg} class="img-fluid" alt="loading..." /></div>
+            <div className="position-absolute title-text">
+              <h1 class="main">Hey Welcome to Helix!</h1>
+              <button class="btn btn-primary btn-md col-xs-2" onClick={logout}>
+                Logout
+              </button>
+              <button
+                className="btn btn-secondary btn-md col-xs-2"
+                onClick={() => {
+                  history.push("/live");
+                }}
+              >
+                Live
+              </button>
+            </div>
+            <div>
+              <img
+                src={helix_bg}
+                class="img-fluid vh-100 vw-100 position-relative"
+                alt="loading..."
+              />
+            </div>
           </div>
         </div>
-    
       </section>
     </>
   );
