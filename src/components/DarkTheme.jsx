b771148112d7a02bfecd9ff0b8333e8250bc0456
Crispin R <crispinr003@gmail.com>
2022-01-07 20:35:25 +0530

    Add theme functionality

---
+++ src/components/DarkTheme.jsx
@@ -0,0 +1,33 @@
+import { setLocalTheme } from "../pages/theme";
+import Button from "@material-ui/core/Button";
+import { useState } from "react";
+
+export default function Toggle() {
+  const [btnImage, setBtnImage] = useState(() => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      return "🌛";
+    } else if (localStorage.getItem("theme") === "theme-light") {
+      return "🌞";
+    } else {
+      return "🌞";
+    }
+  });
+
+  const handleOnClick = () => {
+    if (localStorage.getItem("theme") === "theme-light") {
+      setLocalTheme("theme-dark");
+      setBtnImage("🌛");
+    } else {
+      setLocalTheme("theme-light");
+      setBtnImage("🌞");
+    }
+  };
+
+  return (
+    <>
+      <Button class="themeButton" onClick={handleOnClick}>
+        {btnImage}
+      </Button>
+    </>
+  );
+}
---
+++ src/pages/theme.js
@@ -0,0 +1,16 @@
+export function setLocalTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+
+export function checkTheme() {
+  if (localStorage.getItem("theme")) {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setLocalTheme("theme-dark");
+    } else if (localStorage.getItem("theme") === "theme-light") {
+      setLocalTheme("theme-light");
+    }
+  } else {
+    setLocalTheme("theme-light");
+  }
+}
