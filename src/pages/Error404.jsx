4a6ed314716a1761f1ef1601b7a5d010582bbf3d
Crispin R <crispinr003@gmail.com>
2022-01-07 20:38:59 +0530

    Update error 404 page

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import LaptopLogo from "../assets/images/error.svg";
 
-export default function Error404() {
+function Error404() {
+  function takeHome() {
+    window.location.replace("/");
+  }
   return (
-      <div>
-        Oops we couldn't fetch you that page!!!
-        
-      </div>
-    
+    <>
+      <section
+        className="d-flex justify-content-center align-items-center bg-dark"
+        style={{ height: "100vh" }}
+      >
+        <div className="text-center">
+          <h1 className="stream-text text-white mb-5">
+            Oops! Seems like you are lost
+          </h1>
+          <img src={LaptopLogo} className="" alt="Page not found" />
+          <button
+            onClick={takeHome}
+            className="errorButton mt-5 text-white btn btn-primary"
+            style={{ backgroundColor: "#363636" }}
+          >
+            <span>Take me home</span>
+          </button>
+        </div>
+      </section>
+    </>
   );
 }
-
-
-
+export default Error404;
