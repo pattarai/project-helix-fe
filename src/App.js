ba42a84a5dac7a81994c36644c00df390e52b21f
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-08 21:19:52 +0530

    Removed RecordVids page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Home from "./pages/Home";
 import LiveStream from "./pages/LiveStream";
 import Error404 from "./pages/Error404";
 import PrivateRoute from "./PrivateRoute";
-import RecordedVids from "./pages/RecordedVids";
 import YoutubeEmbed from "./components/YoutubeEmbed";
 
 export default function App() {
@@ -21,7 +20,6 @@ export default function App() {
         <PrivateRoute exact path="/home" component={Home} />
         <Route exact path="/live" component={LiveStream} />
         <Route exact path="/404" component={Error404} />
-        <Route exact path="/record" component={RecordedVids} />
         <Route exact path="/youtubeembed" component={YoutubeEmbed} />
         <Redirect to="/404" />
       </Switch>
