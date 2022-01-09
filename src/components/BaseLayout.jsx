50f34d1f39ee0845a21e6b1bd2676fb21297a592
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-09 09:53:44 +0530

    Updated base layout

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -1,53 +1,29 @@
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import FadeIn from "../animations/FadeIn";
-import firebase from "firebase/app";
+// import firebase from "firebase/app";
 import React from "react";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import Particles from "../animations/Particles";
-import Toggle from "../components/DarkTheme";
-import { Navbar, Nav } from "react-bootstrap";
+import NavBar from "../components/NavBar";
+// import Toggle from "../components/DarkTheme";
+// import { Navbar, Nav } from "react-bootstrap";
 
 export default function BaseLayout(props) {
-  const history = useHistory();
+  // const history = useHistory();
 
-  function logout() {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        history.push("/");
-      });
-  }
+  // function logout() {
+  //   firebase
+  //     .auth()
+  //     .signOut()
+  //     .then(() => {
+  //       history.push("/");
+  //     });
+  // }
 
   return (
     <>
       <FadeIn>
-      <Navbar expand="lg" className="mx-2">
-       <Toggle />
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-          <div class="me-3">
-                <Button
-                  class="customButton"
-                  onClick={() => {
-                    history.push("/live");
-                  }}
-                >
-                  LIVE NOW
-                </Button>
-              </div>
-
-              <div>
-                {" "}
-                <Button class="customButton " onClick={logout}>
-                  LOGOUT
-                </Button>
-              </div>
-
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <NavBar />
         <section className="d-flex justify-content-center align-items-center mt-3">
           <div
             className="card main-bg"
