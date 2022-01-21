580cdd11fe20632cd181245c8a3f60b7c180008b
Crispin R <crispinr003@gmail.com>
2022-01-21 14:00:20 +0530

    Update firebase config as env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,20 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+let { REACT_APP_API_KEY } = process.env;
+let { REACT_APP_AUTH_DOMAIN } = process.env;
+let { REACT_APP_PROJECT_ID } = process.env;
+let { REACT_APP_STORAGE_BUCKET } = process.env;
+let { REACT_APP_MESSAGING_SENDER_ID } = process.env;
+let { REACT_APP_APP_ID } = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAuoEKF1s3Yhb7nlHfsz6PSm_rQhcMtrIg",
-  authDomain: "login-a5b42.firebaseapp.com",
-  projectId: "login-a5b42",
-  storageBucket: "login-a5b42.appspot.com",
-  messagingSenderId: "897626566560",
-  appId: "1:897626566560:web:531602b4cf511a95b27ec5",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
